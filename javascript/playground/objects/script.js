d4d77e0505668df20174c3f279f1f92b49803684
Eric Sandstrom <eric@flexify.se>
2016-12-05 11:38:13 +0100

    added objects folder

---
+++ javascript/playground/objects/script.js
@@ -0,0 +1,102 @@
+//QUESTION 1;
+function questionOne() {
+    'use strict';
+
+    var car = {
+        color: "red",
+        type: "suv",
+        size: "small"
+    };
+
+    car.doors = "3";
+
+    console.log(car);
+}
+
+//Question 2;
+function questionTwo() {
+    'use strict';
+    var myArray = ["matches", "knife", "accelerant", "cedar wood", "stick", "tinder plug"];
+    var myObj = {};
+    var i;
+    for (i = 0; i < myArray.length; i = i + 1) {
+        myObj[myArray[i]] = 0;
+    }
+    console.log(myObj);
+}
+
+//Question 3;
+function questionThree() {
+    'use strict';
+    var person = {
+        name: "Olof Svensson",
+        weight: 86,
+        eatDoritos: function (times) {
+            var changeWeight = this.weight + (0.5 * times);
+            this.weight = changeWeight;
+            return changeWeight;
+        }
+    };
+    person.eatDoritos(2);
+    console.log(person);
+}
+
+//Question 4;
+function questionFour() {
+    'use strict';
+    var contact = [];
+    var persons = {
+        person1: {
+            name: "Stefan",
+            address: "abc",
+            phone: "123"
+        },
+        person2: {
+            name: "Lisa",
+            address: "def",
+            phone: "456"
+        },
+        person3: {
+            name: "Adolf",
+            address: "ghi",
+            phone: "789"
+        }
+    };
+    var prop, i, j;
+    var objSize = 0;
+    var objProp = [];
+    for (prop in persons) {
+        objSize = objSize + 1;
+        objProp.push(prop);
+    }
+    for (i = 0; i < objSize; i = i + 1) {
+        contact.push(persons[objProp[i]]);
+    }
+    for (j = 0; j < contact.length; j = j + 1) {
+        console.log("");
+        console.log("Contact " + j);
+        console.log("Name: " + contact[j].name);
+        console.log("Address: " + contact[j].address);
+        console.log("Phone: " + contact[j].phone);
+    }
+}
+
+
+console.log("Question 1");
+questionOne();
+
+console.log("");
+
+console.log("Question 2");
+questionTwo();
+
+console.log("");
+
+console.log("Question 3");
+questionThree();
+
+console.log("");
+
+console.log("Question 4");
+questionFour();
+
